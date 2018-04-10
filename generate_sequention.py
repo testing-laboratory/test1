2c761eec41c5c28b66352942160d4cd58103005e
Sergey Kolomenkin <kolomenkin@gmail.com>
2018-04-10 07:55:38 +0300

    more heavy pages

--- generate_sequention.py
+++ generate_sequention.py
@@ -2,20 +2,19 @@ import sys
 
 tag = "span"
 
-for x in range(0, 125000):
-	sys.stdout.write("<" + tag + ">")
-	if((x+1) % 1000 == 0):
+for x in range(0, 62500):
+	sys.stdout.write("<" + tag + "></" + tag + ">")
+	if((x+1) % 500 == 0):
 		sys.stdout.write("\n")		
-		
 
 sys.stdout.write("\n")		
-sys.stdout.write("my deep deep text")
+sys.stdout.write("the middle of text")
 sys.stdout.write("\n")		
 sys.stdout.write("\n")		
 
-for x in range(0, 125000):
-	sys.stdout.write("</" + tag + ">")
-	if((x+1) % 1000 == 0):
+for x in range(0, 62500):
+	sys.stdout.write("<" + tag + "></" + tag + ">")
+	if((x+1) % 500 == 0):
 		sys.stdout.write("\n")		
 
 sys.stdout.write("\n")		
